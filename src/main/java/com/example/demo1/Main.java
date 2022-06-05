9e66a39617e4a93a59c16ca281b2f981b5ce0912
nie_Igi_konto <jakub,dobosz01@gmail.com>
2022-06-06 01:35:48 +0200

    pierwsze zadanie

--- src/main/java/com/example/demo1/Main.java
+++ src/main/java/com/example/demo1/Main.java
@@ -6,15 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Database db = Database.getInstance();
 
+        // Generowanie soli a następnie wykonanie funkcji skrótu na haśle i soli
         String salt = User.generateSalt();
         String hash = User.generateHash("1234", salt);
+
+        // Tworzenie nowej instancji klasy User
         User user1 = new User("user1", "Kornel", "Ciepotka", hash, salt);
+
+        // Utworzenie obiektu JSON, który następnie umieścimy w bazie danych
         JSONObject jo1 = db.jsonifyUser(user1);
 
+        // Stworzenie obiektu JSON, który będzie przechowywał wszystkie obiekty reprezentujące urzytkowników
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("jo1", jo1);
 
         db.writeJsonObject("Users", jsonObject);
+
+
 
         // Na podstawie powyższego kodu stwórz i dodaj do bazy danych instancję klasy Contestant
         // Zwróć uwagę na metodę jsonifyUser() w klasie Database, będzie ona pomocna przy wykonywaniu zadania
