36765b38f708ede86ccecf0a9ce8169a49d4a16c
jopajak <jopajak@student.agh.edu.pl>
2022-06-06 09:44:56 +0200

    added gitignore

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
     }
 
     public static void main(String[] args) {
